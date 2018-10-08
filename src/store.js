09d430ec55821ccb9539793f4f5be9745e92f65b
marksabelita <marksabelita@gmail.com>
2018-10-08 16:28:39 +0800

    Fixed unused dependencies

--- src/store.js
+++ src/store.js
@@ -3,21 +3,17 @@ import { createLogger } from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import createHistory from "history/createBrowserHistory";
 import { routerMiddleware } from "react-router-redux";
-import createSagaMiddleware from 'redux-saga';
 
 import reducer from './reducers';
-import rootSaga from './sagas';
 
 const history = createHistory();
 const middleware = routerMiddleware(history);
-const sagaMiddleware = createSagaMiddleware();
 
 const getMiddleware = () => {
   if (process.env.NODE_ENV === 'production') {
-    return applyMiddleware(middleware, sagaMiddleware);
+    return applyMiddleware(middleware);
   }
-  return applyMiddleware(middleware, createLogger(), sagaMiddleware);
+  return applyMiddleware(middleware, createLogger());
 };
 
 export default createStore(reducer, composeWithDevTools(getMiddleware()));
-sagaMiddleware.run(rootSaga);
